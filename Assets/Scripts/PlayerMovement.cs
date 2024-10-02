e28e171e9c84942d5795e1ce45c80465017af5e4
MarsPH <m.poorhamidian@gmail.com>
2024-10-02 15:53:34 -0400

    Added PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float movementSpeed = 5f;
+
+    private Camera _cam;
+    private Collider _floorCollider;
+    private RaycastHit _hit;
+
+    private void Start()
     {
-        
+        _cam = GameObject.Find("Main Camera").GetComponent<Camera>(); //it will set Camera component in obj camera to _cam
+        _floorCollider =
+            GameObject.Find("Floor").GetComponent<Collider>(); // it will set collider of floor to the variable
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!Input.GetMouseButton(0)) return;
+        Ray ray = _cam.ScreenPointToRay(Input.mousePosition); // it will make a ray from the camera to the position of mouse
+        Physics.Raycast(ray, out _hit); //calculate the raycast of ray and store date in _hit
+
+        if (_hit.collider != _floorCollider) return; // one of _hit data is the collider that hit
+
+        transform.position = Vector3.MoveTowards(transform.position, _hit.point, Time.deltaTime * movementSpeed); // For moving from obj
+        transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));// For direction
+
     }
 }
